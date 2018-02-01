589f8c7939c86317b816aae3afc071d8b09f08c8
Andrey Krivoshapov <andrey.krivoshapov@lykke.com>
2018-02-01 14:34:39 +0300

    Remove not necessary settings

--- src/Lykke.Job.IcoInvestment.Core/Settings/JobSettings/IcoInvestmentSettings.cs
+++ src/Lykke.Job.IcoInvestment.Core/Settings/JobSettings/IcoInvestmentSettings.cs
@@ -7,8 +7,6 @@ namespace Lykke.Job.IcoInvestment.Core.Settings.JobSettings
         public AzureQueueSettings AzureQueue { get; set; }
         public DbSettings Db { get; set; }
         public String IcoExRateServiceUrl { get; set; }
-        public String KycServiceUrl { get; set; }
-        public String KycServiceCampaignId { get; set; }
         public String KycServiceEncriptionKey { get; set; }
         public String KycServiceEncriptionIv { get; set; }
         public String SiteSummaryPageUrl { get; set; }
